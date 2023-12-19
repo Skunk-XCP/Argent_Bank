91f1f589fd75c0c34971355e5b8d747235087c8a
Skunk-XCP <skunk-xcp@hotmail.fr>
2023-12-19 16:53:11 +0100

    first commit

---
+++ src/scenes/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import s from "./style.module.css";
+
+export function HomePage() {
+   return (
+      <>
+         <header></header>
+         <main>
+            <h1 className={s.title}>Argent Bank</h1>
+         </main>
+      </>
+   );
+}
